536c455f83e3a29d40d4faf7b2fd189c1930c753
Jurgen Verhasselt <sjugge@inuits.eu>
2024-04-15 16:10:50 +0200

    [PROXYSQL] workaround for race condition

--- manifests/reload_config.pp
+++ manifests/reload_config.pp
@@ -9,7 +9,12 @@ class proxysql::reload_config {
 
   $mycnf_file_name = $proxysql::mycnf_file_name
   exec { 'reload-config':
-    command     => "/usr/bin/mysql --defaults-extra-file=${mycnf_file_name} --execute=\"
+    # Prepended the executed mysql command with a sleep command to avoid a race condition which was reached consistently during testing
+    # when updating both user and server configuration. This could leave ProxySQL with reconfigured server but outdated user configuration
+    # as the mysql command could not be executed succesfully briefly after updating the server configuration. Adding a brief delay to
+    # this exec effectivly seems to resolves this, side-stepping the need for 2 puppet runs to apply all configuration changes.
+    # It's not pretty, but it seems to effectivly and consistencly avoid the issue.
+    command     => "/usr/bin/sleep 1 && /usr/bin/mysql --defaults-extra-file=${mycnf_file_name} --execute=\"
           LOAD ADMIN VARIABLES FROM CONFIG; \
           LOAD ADMIN VARIABLES TO RUNTIME; \
           SAVE ADMIN VARIABLES TO DISK; \
